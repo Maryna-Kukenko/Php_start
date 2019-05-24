e4ce5628cdf188daf554749e82e5c8985f4d2d20
Maryna.Kykenko <tooshcanchik@gmail.com>
2019-05-24 18:36:26 +0300

    update fibonacci function

--- fibonacci/index.php
+++ fibonacci/index.php
@@ -6,14 +6,15 @@ $position = 8;
 function fibonacci($position, $first, $second){
   $value = $first + $second;
 
-  if ($position > 3) {
+  if ($position > 0) {
+    echo $first."<br>";
+
     $first = $second;
     $second = $value;
-    echo $value." ";
+
     return fibonacci($position - 1, $first, $second);
   }
-  echo "<br>";
-  return "Fibonacci number on position {$position} = {$value}";
+  return null;
 }
 
 echo fibonacci($position, $firstNumber, $secondNumber);
