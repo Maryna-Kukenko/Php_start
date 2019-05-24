f61536954a054fb4f855b2c7be1b539f6d2cfeac
Maryna.Kykenko <tooshcanchik@gmail.com>
2019-05-24 18:01:54 +0300

    update text description

--- fibonacci/index.php
+++ fibonacci/index.php
@@ -1,7 +1,7 @@
 <?php
 $firstNumber = 0;
 $secondNumber = 1;
-$position = 25;
+$position = 8;
 
 function fibonacci($position, $first, $second){
   $value = $first + $second;
@@ -9,10 +9,11 @@ function fibonacci($position, $first, $second){
   if ($position > 3) {
     $first = $second;
     $second = $value;
+    echo $value." ";
     return fibonacci($position - 1, $first, $second);
   }
-
-  return $value;
+  echo "<br>";
+  return "Fibonacci number on position {$position} = {$value}";
 }
 
 echo fibonacci($position, $firstNumber, $secondNumber);
